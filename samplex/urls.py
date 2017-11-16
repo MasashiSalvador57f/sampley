1ef5c9fb6070a458fe7119a7e7ee4f3d4286dc33
Masashi Salvador Mitsuzawa <masashisalvador57f@gmail.com>
2017-11-16 13:27:02 +0900

    sample

--- samplex/urls.py
+++ samplex/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import hello.views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/', hello.views.main),
 ]
